5e5252b6322df60353d4d454a61be9b4aea26218
Ali <am_alizadeh74@yahoo.com>
2023-05-26 21:25:40 +0200

    friend request notification Added

---
+++ app/src/main/java/com/example/profilelab/FireNotification.kt
@@ -0,0 +1,35 @@
+package com.example.profilelab
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class FireNotification(var context: Context, var title: String, var message: String) {
+    private val channelID: String = "MADFCM100"
+    private val channelName: String = "FCM Message 100"
+    private val notificationManager: NotificationManager = context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: NotificationCompat.Builder
+
+    fun fireNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val intent = android.content.Intent(context, FriendsContainer::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        builder = NotificationCompat.Builder(context, channelID)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_send)
+            .addAction(R.drawable.ic_send, "See Requests", pendingIntent)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+        notificationManager.notify(100, builder.build())
+    }
+
+
+}
--- app/src/main/java/com/example/profilelab/Friends.kt
+++ app/src/main/java/com/example/profilelab/Friends.kt
@@ -1,6 +1,12 @@
 package com.example.profilelab
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,21 +29,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -52,17 +53,19 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.profilelab.models.FireUser
 import com.example.profilelab.ui.theme.ProfileLabTheme
 import com.example.profilelab.view_models.FireUserViewModel
-import com.example.profilelab.view_models.Friend
-import com.example.profilelab.view_models.FriendRequest
-import com.example.profilelab.view_models.FriendsViewModel
-import com.google.common.io.Files.append
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 class Friends : ComponentActivity() {
@@ -114,7 +117,7 @@ class Friends : ComponentActivity() {
                             items = usersList.value,
                             key = { it.id }
                         ) {
-                            FriendCard(emp = it, usersVM)
+                            FriendCard(emp = it, usersVM = usersVM)
                         }
                     }
                 }
@@ -185,13 +188,6 @@ fun FriendCard(emp: FireUser, usersVM: FireUserViewModel){
                         fontSize = 15.sp
                     )
                 )
-//                Text(
-//                    text = "Email : ${emp.username}",
-//                    style = TextStyle(
-//                        color = Color.Black,
-//                        fontSize = 15.sp
-//                    )
-//                )
 
                 Spacer(modifier = Modifier.padding(5.dp))
                 Row() {
@@ -235,6 +231,12 @@ fun FriendCard(emp: FireUser, usersVM: FireUserViewModel){
             ),
             onClick = {
                 usersVM.sendFriendRequest(emp)
+                val notice = FireNotification(
+                    mContext,
+                    title = "Friend Request",
+                    message = "Your Friend Request sent to : ${emp.nickname}",
+                )
+                notice.fireNotification()
             }) {
             Text("Connect +")
         }
